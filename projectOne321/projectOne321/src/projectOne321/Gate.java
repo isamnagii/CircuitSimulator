c2395f681cfcf96165ef995df49ccd0737094bdb
isamnagii <78992274+isamnagii@users.noreply.github.com>
2021-10-17 15:38:49 -0400

    Added files for github use

---
+++ projectOne321/projectOne321/src/projectOne321/Gate.java
@@ -0,0 +1,59 @@
+package projectOne321;
+
+public class Gate {
+	int x = 1;
+	int y = 1;
+	String j;
+	String k;
+	int m = 1;
+	int n = 1;
+/*	public Gate(int setN, int setM) {
+		setM(setM);
+		setN(setN);
+	}
+	*/
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public String getJ() {
+		return j;
+	}
+	public void setJ(String j) {
+		this.j = j;
+	}
+	public String getK() {
+		return k;
+	}
+	public void setK(String k) {
+		this.k = k;
+	}
+	public int getM() {
+		return m;
+	}
+	public void setM(int m) {
+		this.m = m;
+	}
+	public int getN() {
+		return n;
+	}
+	public void setN(int n) {
+		this.n = n;
+	}
+	
+	public Gate gate(int m, int n) {
+		
+		return null;
+		
+	}
+	
+	
+}
